afc79d18f3decfb1216dd6b9172308887e3e7b4c
lykhtar <55245146+lykhtar@users.noreply.github.com>
2019-09-20 19:25:33 +0800

    Update LabW2_2.java

--- LabW2_2.java
+++ LabW2_2.java
@@ -1,39 +1,40 @@
-package labw2_1;
+package labw2_2;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class LabW2_1 {
+public class LabW2_2 {
 
     private static BufferedReader br = null;
     private static String st = null;
     private static int p = 0;
     private static String a, b;
     private static String[] s = null;
+    static String pat = "public";
+    static String pat1 = "private";
 
     private static void Work() throws FileNotFoundException, IOException {
-        br = new BufferedReader(new FileReader("D:\\КПО\\LabW2_1\\z1.txt"));
+        br = new BufferedReader(new FileReader("D:\\КПО\\LabW2_2\\LabW2_1.txt"));
+
         String tmp = "";
         while ((tmp = br.readLine()) != null) {// пробел использовать как разделитель 
             s = tmp.split("\\s");// вывести полученне строки 
             p = s.length;
+
             for (int t = 1; t < p; t++) {  //поделить на отдельные слова
-                String res = s[t - 1];//взять первое слово
-                String res1 = s[t];//взять следующее за ним слово
-                char[] la = res.toCharArray();//разбить первое слово на символы
-                char[] la1 = res1.toCharArray();//разбить второе слово на символы
-                a = Character.toString(la[res.length() - 1]);//перевести последний символ первого слова в троку
-                b = Character.toString(la1[0]);//перевести первый символ сторого слова в строку
-                if (a.equals(b)) {//если последний и первый строки равны
-                    System.out.println(s[t - 1]);//вывести первое слово
-                }
+                System.out.println(s[t]);
+                Files.write(path, new String(Files.readAllBytes(path), StandardCharsets.UTF_8).replace(pat, pat1).getBytes(StandardCharsets.UTF_8));
             }
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) {
         try {
             Work();
         } catch (IOException e) {
